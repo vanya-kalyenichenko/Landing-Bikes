22323023ac54352bbdacc5718592ccfcbf84e950
Ivan <your@mail.com>
2024-04-15 12:07:01 +0300

    make build

---
+++ index.d3e89eb2.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.querySelector("form");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()});
+//# sourceMappingURL=index.d3e89eb2.js.map
